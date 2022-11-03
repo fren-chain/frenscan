f242dfc7d6cfcd0140caa329f2d4181b53b48000
root <root@fra-sm5037mc-h12trf-6-6.gthost.com>
2022-11-03 19:07:54 +0000

    frenscan genesis

---
+++ apps/indexer/config/prod/geth.exs
@@ -0,0 +1,20 @@
+use Mix.Config
+
+config :indexer,
+  block_interval: :timer.seconds(5),
+  json_rpc_named_arguments: [
+    transport: EthereumJSONRPC.HTTP,
+    transport_options: [
+      http: EthereumJSONRPC.HTTP.HTTPoison,
+      url: System.get_env("ETHEREUM_JSONRPC_HTTP_URL") || "https://evm.cryptocurrencydevs.org",
+      http_options: [recv_timeout: :timer.minutes(1), timeout: :timer.minutes(1), hackney: [pool: :ethereum_jsonrpc]]
+    ],
+    variant: EthereumJSONRPC.Geth
+  ],
+  subscribe_named_arguments: [
+    transport: EthereumJSONRPC.WebSocket,
+    transport_options: [
+      web_socket: EthereumJSONRPC.WebSocket.WebSocketClient,
+      url: System.get_env("ETHEREUM_JSONRPC_WS_URL") || "wss://localhost:8546"
+    ]
+  ]
